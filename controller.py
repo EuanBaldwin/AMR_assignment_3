e6b1b9d5b75133bf0c046963a3a6c2cfa14d735f
Euan Baldwin <euanebaldwin@gmail.com>
2025-03-21 15:53:55 +0000

    Inital Commit

---
+++ controller.py
@@ -0,0 +1,8 @@
+# Implement a controller
+def controller(state, target_pos, dt):
+    # state format: [position_x (m), position_y (m), position_z (m), roll (radians), pitch (radians), yaw (radians)]
+    # target_pos format: (x (m), y (m), z (m), yaw (radians))
+    # dt: time step (s)
+    # return velocity command format: (velocity_x_setpoint (m/s), velocity_y_setpoint (m/s), velocity_z_setpoint (m/s), yaw_rate_setpoint (radians/s))
+    output = (0, 0, 0, 0)
+    return output
